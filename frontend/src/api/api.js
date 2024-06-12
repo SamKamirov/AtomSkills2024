119bac0e0bd8f0c0adb60c685934bd4fceaeba7f
Sam <semkamirov@gmail.com>
2024-06-12 21:53:20 +0500

    Меняет адрес сервера

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const createApi = () => {
   const api = axios.create({
-    baseURL: 'http://localhost:8082/api',
+    baseURL: 'http://45.86.181.61:8082/api',
     timeout: 5000
   });
 
